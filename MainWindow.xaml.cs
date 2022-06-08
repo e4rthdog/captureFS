fae076700f742533ab28eba18988654bdcbcdfef
Elias Stassinos <elias.stassinos@gmail.com>
2022-06-08 09:49:16 +0300

    -Upgrade to .NET 6.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using System.ComponentModel;
